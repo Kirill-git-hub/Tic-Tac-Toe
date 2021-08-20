5d0afb210c7f752390c82ab013831db7d0e8956b
Kirill Vdovin <kirilvdovinkv@gmail.com>
2021-08-20 21:49:17 +0300

    - added script to start and finish the game

--- Assets/Scripts/HomeMenuController.cs
+++ Assets/Scripts/HomeMenuController.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HomeMenuController : MonoBehaviour
 {
+    [SerializeField] private Button[] chooseSideButton;
+    [SerializeField] private Button exitButton;
+    [SerializeField] private GameObject gamePanel; 
+    [SerializeField] private GameObject menuPanel; 
+    
     // Start is called before the first frame update
     void Start()
     {
+        FinishGame();
         
+        for (int i = 0; i < chooseSideButton.Length; i++)
+        {
+            chooseSideButton[i].onClick.AddListener(() => StartGame());
+        }
+        
+        exitButton.onClick.AddListener((() => FinishGame()));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartGame()
     {
-        
+        menuPanel.gameObject.SetActive(false);
+        gamePanel.gameObject.SetActive(true);
+    }
+
+    public void FinishGame()
+    {
+        menuPanel.gameObject.SetActive(true);
+        gamePanel.gameObject.SetActive(false);
     }
 }
