cc3c62a52003286f74f00aa2bb69d7f8ea95b5fb
Kirill Vdovin <kirilvdovinkv@gmail.com>
2021-08-23 22:02:05 +0300

    - refactoring code in HomeMenuController script

--- Assets/Scripts/HomeMenuController.cs
+++ Assets/Scripts/HomeMenuController.cs
@@ -8,28 +8,42 @@ public class HomeMenuController : MonoBehaviour
     [SerializeField] private Button ButtonX;
     [SerializeField] private Button ButtonO;
     [SerializeField] private Button exitButton;
+    [SerializeField] private Button restartButton;
     [SerializeField] private GameObject gamePanel; 
-    [SerializeField] private GameObject menuPanel; 
-    
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameController gameController;
+
     // Start is called before the first frame update
     void Start()
     {
-        FinishGame();
+        gameController.PlayerSide = PlayerType.Empty;
         
-        ButtonX.onClick.AddListener(() => StartGame());
-        ButtonO.onClick.AddListener(() => StartGame());
-        exitButton.onClick.AddListener((() => FinishGame()));
+        SwitchPanels();
+        
+        exitButton.onClick.AddListener(() =>
+        {
+            gameController.FinishGame();
+            SwitchPanels();
+        });
+        
+        ButtonX.onClick.AddListener(() =>
+        {
+            gameController.SetStartingSide(PlayerType.Cross);
+            SwitchPanels();
+        });
+        
+        ButtonO.onClick.AddListener(() =>
+        {
+            gameController.SetStartingSide(PlayerType.Zero);
+            SwitchPanels();
+        });
+        
+        restartButton.onClick.AddListener(gameController.RestartGame);
     }
 
-    public void StartGame()
+    public void SwitchPanels()
     {
-        menuPanel.gameObject.SetActive(false);
-        gamePanel.gameObject.SetActive(true);
-    }
-
-    public void FinishGame()
-    {
-        menuPanel.gameObject.SetActive(true);
-        gamePanel.gameObject.SetActive(false);
+        menuPanel.gameObject.SetActive(gameController.PlayerSide == PlayerType.Empty);
+        gamePanel.gameObject.SetActive(gameController.PlayerSide != PlayerType.Empty);
     }
 }
