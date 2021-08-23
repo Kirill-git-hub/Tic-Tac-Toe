ad6c43204436421d2c883c316d33e02351908678
Kirill Vdovin <kirilvdovinkv@gmail.com>
2021-08-23 21:47:11 +0300

    - created WinningCombinations class to set up all winning combinations in incpector

---
+++ Assets/Scripts/WinningCombination.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class WinningCombination
+{
+    public GameButton firstItem;
+    public GameButton secondItem;
+    public GameButton thirdItem;
+}
