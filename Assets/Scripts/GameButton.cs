544eb02b1f3edf614c102ef74664dcd7186a83b0
Kirill Vdovin <kirilvdovinkv@gmail.com>
2021-08-23 21:42:27 +0300

    - created GameButton script, Init method to initialize button when we move, ResetButton method to clean game field on starting a new game

---
+++ Assets/Scripts/GameButton.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class GameButton : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private Image buttonImage;
+    [SerializeField] private GameController gameController;
+    
+    private PlayerType occupiedBy = PlayerType.Empty;
+    
+    public Button Button => button;
+    public PlayerType OccupiedBy => occupiedBy;
+
+    public void Init(PlayerType playerType, Sprite imageSprite)
+    {
+        Color tempColor = buttonImage.color;
+        tempColor.a = 1f;
+        buttonImage.color = tempColor;
+        
+        buttonImage.sprite = imageSprite;
+        button.interactable = false;
+        occupiedBy = playerType;
+        gameController.MovesCount++;
+
+        gameController.CheckWinner();
+        
+        if (gameController.IsPlayersTurn)
+        {
+            gameController.IsPlayersTurn = false;
+            if (!gameController.HasWinner)
+            {
+                gameController.AIController.MakeStep();
+            }
+        }
+    }
+
+    public void ResetButton()
+    {
+        Color tempColor = buttonImage.color;
+        tempColor.a = 0f;
+        buttonImage.color = tempColor;
+        
+        button.interactable = true;
+        buttonImage.sprite = null;
+        occupiedBy = PlayerType.Empty;
+        button.onClick.RemoveAllListeners();
+    }
+    
+    
+}
